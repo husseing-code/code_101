244c2d3296c813ddc2d2fd8659068110fcc2353c
husseing-code <hussein.gure@audienceview.com>
2020-09-20 10:24:37 -0400

    new commit

---
+++ ENCOR_python/collections.py
@@ -0,0 +1,22 @@
+fruits = ["apple", "banana", "strawberry", "orange", "peach"]
+
+fruits[4] = "nectarine"
+
+print(fruits[4])
+
+#a type object, just like a list but 'immutable'
+fruits_tuple = ("apple", "banana", "strawberry", "orange", "peach")
+
+#This line will throw an exception
+#fruits_tuple[4] = "Nectarine"
+
+
+fruit_locations = {
+    "Bin 1" : "apple",
+    "Bin 2" : "banana",
+    "Bin 3" : "strawberry",
+    "Bin 1" : "orange",
+    "Bin 1" : "peach",
+}
+
+print(fruit_locations.get("Bin 3"))
