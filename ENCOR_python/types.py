fd906a7a5817bd93e3a7dc1ef7858aa79560d0af
husseing-code <hussein.gure@audienceview.com>
2020-09-20 10:14:25 -0400

    third commit

---
+++ ENCOR_python/types.py
@@ -0,0 +1,17 @@
+firstname = "Hussein"
+lastname = "Gure"
+age = 41
+married = False
+height = 68.7
+
+print("The type of the firstname variable is: " + str(type(firstname)))
+
+firstname = 200
+
+print("The type of the firstname variable is: " + str(type(firstname)))
+
+print("The type of the age variable is: " + str(type(age)))
+
+print("The type of the married variable is: " +str(type(married)))
+
+print("The type of the height variable is: " + str(type(height)))
